d4d52b25d316f50e6f8852ad945ecec65b243d12
jennymhkao <k.jennymh@gmail.com>
2020-08-13 00:08:51 -0700

    update file

--- deleteFiles.py
+++ deleteFiles.py
@@ -2,23 +2,27 @@
 # deleteFiles.py - deletes unneeded files. 
 
 import os
-# only if truly deleting - import shutil, send2trash 
+# only if truly deleting - import send2trash 
 
 # Ask what directory to search.
 search_dir = input('What directory do you want me to search?')
 root = os.path.abspath(search_dir)
 
+pathList = {}
+
 if not os.path.exists(root):
     print("Provide valid source folder")
 else:
-    # Walks through folder tree using os.listdir().
+    # Walk through folder tree using os.listdir().
     for filename in os.listdir(root):
         filepath = os.path.join(root, filename)
 
         # Get file size
-        size = 0
-        size += os.path.getsize(filepath)
-
+        pathSize = os.path.getsize(filepath)
+        if pathSize < 100000000:
+            continue
+        os.send2trash(filepath)
+        print('Deleting' + filename + '...')
     
 
 
